7cc46839d1f0179e4e22d8597a4b9c5492a99fc6
addshore <addshorewiki@gmail.com>
2016-05-03 12:44:01 +0100

    Also add module in onDiffViewHeader hook
    Change-Id: I33f85d0e24092b1e5bf9577069a3c34b9ac3e7a4

--- RevisionSlider.hooks.php
+++ RevisionSlider.hooks.php
@@ -8,15 +8,6 @@
  * @license GPL-2.0+
  */
 class RevisionSliderHooks {
-	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-		if ( self::shouldLoadRevisionSlider( $skin->getContext()->getRequest() ) ) {
-			$out->addModules( 'ext.RevisionSlider.init' );
-		}
-	}
-
-	private static function shouldLoadRevisionSlider( WebRequest $request ) {
-		return $request->getCheck( 'diff' );
-	}
 
 	public static function onDiffViewHeader(
 		DifferenceEngine $diff,
@@ -24,8 +15,8 @@ class RevisionSliderHooks {
 		Revision $newRev
 	) {
 		$out = RequestContext::getMain()->getOutput();
-		$out->addHTML( '<div id="revision-slider-container" style="min-height: 150px;"></div>' );
-
+		$out->addModules( 'ext.RevisionSlider.init' );
+		$out->addHTML( '<div id="revision-slider-container" style="min-height: 150px;">' );
 		$placeHolder = ( new Message( 'revisionslider-loading-placeholder' ) )->parse();
 		$out->addHTML(
 			'<p id="revision-slider-placeholder" style="text-align: center">' .  $placeHolder. '</p>'
