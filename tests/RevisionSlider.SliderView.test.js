44904e868779d186d509f210c6758c0ddb9fc1bb
addshore <addshorewiki@gmail.com>
2016-05-12 17:47:53 +0100

    Avoid changing the URL during SliderView test
    This was super annoying as if you went to refresh
    the test page to run the tests again it would take
    you to some broken diff page instead...
    
    Change-Id: I43ca8edd005aa3ef72b74a5d812cbe39bccaf332

--- tests/RevisionSlider.SliderView.test.js
+++ tests/RevisionSlider.SliderView.test.js
@@ -2,9 +2,19 @@
 	var SliderView = mw.libs.revisionSlider.SliderView,
 		Slider = mw.libs.revisionSlider.Slider,
 		Revision = mw.libs.revisionSlider.Revision,
-		RevisionList = mw.libs.revisionSlider.RevisionList;
+		RevisionList = mw.libs.revisionSlider.RevisionList,
+		startHistoryState, startHref;
 
 	QUnit.module( 'ext.RevisionSlider.SliderView' );
+
+	QUnit.testStart( function () {
+		startHistoryState = history.state;
+		startHref = window.location.href;
+	} );
+
+	QUnit.testDone( function () {
+		history.replaceState( startHistoryState, 'QUnit', startHref );
+	} );
 
 	QUnit.test( 'render', function ( assert ) {
 		var $container = $( '<div/>' ),
