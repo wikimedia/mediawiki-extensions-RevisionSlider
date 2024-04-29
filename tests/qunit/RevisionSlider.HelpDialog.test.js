363f24a4096175784b57a3108ae65a7d4eef922a
thiemowmde <thiemo.kreuz@wikimedia.de>
2024-04-29 11:58:06 +0200

    Remove dead code from HelpDialog test
    There are not even any <a> elements involved because the language is
    set to qqx.
    
    Change-Id: I64a6c414ccb327bc65a606ebd1579b579cd3215a

--- tests/qunit/RevisionSlider.HelpDialog.test.js
+++ tests/qunit/RevisionSlider.HelpDialog.test.js
@@ -5,38 +5,26 @@ QUnit.test( 'Initialize HelpDialog', function ( assert ) {
 	const helpDialog = new HelpDialog(),
 		windowManager = new OO.ui.WindowManager();
 
-	function getSlideTextHtml( slide ) {
-		return slide.$element.find( '.mw-revslider-help-dialog-text' ).html();
-	}
-
-	function addLinkTargets( message ) {
-		const $container = $( '<div>' ).append(
-			// eslint-disable-next-line mediawiki/msg-doc
-			mw.message( message ).parseDom()
-		);
-		$container.find( 'a' ).attr( 'target', '_blank' );
-		return $container.html();
-	}
-
 	$( document.body ).append( windowManager.$element );
 	windowManager.addWindows( [ helpDialog ] );
 
 	assert.strictEqual( helpDialog.slides.length, 4 );
 	assert.strictEqual( helpDialog.slidePointer, 0 );
+
 	assert.strictEqual(
-		getSlideTextHtml( helpDialog.slides[ 0 ] ),
-		addLinkTargets( 'revisionslider-help-dialog-slide1' )
+		helpDialog.slides[ 0 ].$element.find( '.mw-revslider-help-dialog-text' ).text(),
+		'(revisionslider-help-dialog-slide1)'
 	);
 	assert.strictEqual(
-		getSlideTextHtml( helpDialog.slides[ 1 ] ),
-		addLinkTargets( 'revisionslider-help-dialog-slide2' )
+		helpDialog.slides[ 1 ].$element.find( '.mw-revslider-help-dialog-text' ).text(),
+		'(revisionslider-help-dialog-slide2)'
 	);
 	assert.strictEqual(
-		getSlideTextHtml( helpDialog.slides[ 2 ] ),
-		addLinkTargets( 'revisionslider-help-dialog-slide3a' )
+		helpDialog.slides[ 2 ].$element.find( '.mw-revslider-help-dialog-text' ).text(),
+		'(revisionslider-help-dialog-slide3a)'
 	);
 	assert.strictEqual(
-		getSlideTextHtml( helpDialog.slides[ 3 ] ),
-		addLinkTargets( 'revisionslider-help-dialog-slide4' )
+		helpDialog.slides[ 3 ].$element.find( '.mw-revslider-help-dialog-text' ).text(),
+		'(revisionslider-help-dialog-slide4)'
 	);
 } );
